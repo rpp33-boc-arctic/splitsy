1eea1824f874b789d2f5ea1463eecb1f9b24e6bc
Grant Mitchell <soundbreakr1@gmail.com>
2022-05-20 10:24:06 -0700

    had some github bug where it added the app twice

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 
-const PORT = process.env.PORT || 3000;
-
+const PORT = process.env.PORT || 3001;
+//changed port because react app runs on 3000 as well
+const path = require('path')
 const app = express();
+
+//added by grant m
+app.use(express.static(path.join(__dirname,'../build')));
+//end
 
 app.get("/api", (req, res) => {
   res.json({ message: "Server Status: Active" });
