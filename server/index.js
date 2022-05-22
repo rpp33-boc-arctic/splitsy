a15d93a9e9fc940dd4a022f3562d2576ad041620
Nachcha Aroonrerk (Neen) <neen.nachcha@gmail.com>
2022-05-22 22:20:54 +0700

    Revise jest test to include server create and close in the file

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const PORT = 3000;
-const app = express();
+// const PORT = 3000;
+var app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -10,6 +10,8 @@ app.get("/serverStatus", (req, res) => {
   res.end();
 })
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server listening on ${PORT}`);
+// });
+
+module.exports = app;
--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,35 @@
+const http = require('http');
 const sum = require('./sum');
+var app = require('./server/index.js');
+var PORT = 3000;
+var server;
 
-test('adds 1 + 2 to equal 3', () => {
+beforeAll(async () => {
+  server = await http.createServer(app);
+  server.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+  });
+  // done();
+})
+
+test('adds 1 + 2 to equal 3', (done) => {
   expect(sum(1,2)).toBe(3);
+  done();
 });
+
+test('server is running',  (done) => {
+
+  server.on('listening', () => {
+    var result = server.address().port;
+    console.log(result);
+    expect(result).toBe(3000);
+    expect(result).not.toBe(2000);
+    done();
+  })
+
+});
+
+
+afterAll( function () {
+  server.close();
+})
