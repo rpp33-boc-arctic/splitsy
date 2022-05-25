0f545cd76945fbec00a6a46f7cb27b45c0847f01
yfcheng1 <b99501048@gmail.com>
2022-05-24 23:10:11 -0400

    add controller in server

---
+++ server/controllers/session/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  createSession: (req, res) => {
+
+  }
+}
--- server/routes.js
+++ server/routes.js
@@ -1,42 +1,44 @@
 const router = require('express').Router();
-// const controllers = require('./controllers');
-
+const authController = require('./controllers/authentication');
+const userController = require('./controllers/user');
+const cartController = require('./controllers/cart');
+const restaurantController = require('./controllers/restaurant');
+const sessionController = require('./controllers/session');
+const paymentController = require('./controllers/payment');
 
 // Authentication
-router.post('/register', (req, res) => {
-});
+router.post('/register', authController.register);
 
 router.post('/login', (req, res) => {
 });
 
 
 // User profile
+router.get('/user', userController.profile);
 router.post('/logout', (req, res) => {
 });
 router.get('/user/history', (req, res) => {
-});
-router.get('/user', (req, res) => {
 });
 router.get('/user/friends', (req, res) => {
 });
 
 
 // Restaurants
-router.get('/restaurantList', (req, res) => {
-});
+router.get('/restaurant', restaurantController.restaurantList);
 router.get('/:restaurant/menu', (req, res) => {
 });
 
 
 // Session
-router.post('/session:id', (req, res) => {
-});
+router.post('/session:id', sessionController.createSession);
+
 
 // Cart
-router.post('/session:id/order', (req, res) => {
-});
+router.post('/session:id/order', cartController.order);
+
 
 // Checkout
+router.get('/orderStatus', paymentController.orderStatus);
 router.post('/item:id/add', (req, res) => {
 });
 
@@ -49,8 +51,6 @@ router.get('/user_id/cart', (req, res) => {
 router.get('/pay', (req, res) => {
 });
 
-router.get('/status', (req, res) => {
-});
 
 
 module.exports = router;
