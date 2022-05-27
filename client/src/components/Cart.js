7e6fd67b1c2855834ba923c90cc9f38c372928f5
Tan Ha <79884390+tandha@users.noreply.github.com>
2022-05-26 18:48:18 -0700

    initial skeleton for cart cont.

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -10,20 +10,21 @@ class Cart extends React.Component {
   render() {
     return (
       <div >
-      Cart
+      <div>Cart</div>
       <div>Order Code: XPZ947</div>
       <div>Item 1</div>
       <div>Item 2</div>
       <div>Item 3</div>
       <div>Item 4</div>
       <div>Item 5</div>
-      </div >
+
       <div>Subtotal:      22.97</div>
       <div>Tax (10%):      2.97</div>
       <div>Delivery Fee:      22.97</div>
       <div>Total:      22.97</div>
       <button>Ready</button>
-      <button>Checkout</button>.
+      <button>Checkout</button>
+      </div >
     )
   }
 }
