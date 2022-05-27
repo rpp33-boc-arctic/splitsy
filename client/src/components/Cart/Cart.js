269bceac7d6dde538f766912ca15191971b29df4
Tan Ha <79884390+tandha@users.noreply.github.com>
2022-05-27 16:45:48 -0700

    cart skeleton done!

--- client/src/components/Cart/Cart.js
+++ client/src/components/Cart/Cart.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ButtonAppBar from './navbar.js';
 import OrderCode from './orderCode.js';
 import RedirectButton from './redirectButton.js';
+import ItemList from './itemList.js';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import Summary from './summary.js';
+
 
 class Cart extends React.Component {
   constructor(props) {
@@ -13,20 +17,24 @@ class Cart extends React.Component {
   render() {
     return (
       <div >
-      <ButtonAppBar />
-      <div>Cart</div>
-      <OrderCode />
-      <div>Item 1</div>
+        <ButtonAppBar />
+        <h1>
+          <ShoppingCartIcon />
+          Cart
+          </h1>
+        <OrderCode />
+        {/* <div>Item 1</div>
       <div>Item 2</div>
       <div>Item 3</div>
       <div>Item 4</div>
-      <div>Item 5</div>
-
-      <div>Subtotal:      22.97</div>
-      <div>Tax (10%):      2.97</div>
-      <div>Delivery Fee:      22.97</div>
-      <div>Total:      22.97</div>
-      <RedirectButton />
+      <div>Item 5</div> */}
+        <ItemList />
+        {/* <div>Subtotal:      22.97</div>
+        <div>Tax (10%):      2.97</div>
+        <div>Delivery Fee:      22.97</div>
+        <div>Total:      22.97</div> */}
+        <Summary />
+        <RedirectButton />
       </div >
     )
   }
---
+++ client/src/components/Cart/sampleData.js
@@ -0,0 +1,200 @@
+module.exports =
+	{
+		"quote_ids": [
+			"gAAAAABijcmq_TugBn7-yyGU7iQcqv05kKsuNHdq6xCfAhYaDutAJBBpndBTw7W1Isa_bcDYV50hvazmFBRh61d5kuowPqgd7rz-hy0-V2b__ivKXudLk69fKbHfTBbfSvB9MDU_brhoXQH-M6ApOOcMvIKeloo7d2ufhr7_hCVqG4muoQYitkuUe2ROKDzmTjxx8ANWm5ynaIX4jWGz82b0DPCNDT65MTwn1q1vcBoPaP4GQ5boavn8LchtZiGLsn4QfhRYwJdXesdy4H0wqk7QJWEhlJd7vuK1AMCRPZGWiLsC4E8V2PjypF3P57pnWum22WWGwMMo1a-gByIKjJM0zgRwrRHPVg==",
+			"gAAAAABijcmqpZGzRefM3v1ME1Q7ecn0-NoKUiw76NxHX0inoWfpGmG98dnKAIBpH364y1x8DnUrO5cs_4zALNVU1Xnzv63MnBB9dUQ0NARpCOSYoCUqBOJKiWhozc3jaxyiqmvnT9v8Nc-giUf4OyuXscnCqFvElURolSk8CekMkDH7Egt7iK1FhUZcvpqAh6dOEbmL2UyXPBj8gsE2jApUw6FgoujSAGC744cWCLNFUU5nf-meqmWMtte-NJXCivQnvs8b0Qf7ByEA5MQa2QadbB7o-aos-KRzvEgBdbIqv36escqbE9Q="
+		],
+		"name": "Ihop",
+		"phone_number": 16233628002,
+		"address": {
+			"street_addr": "9976 W Happy Valley Pkwy",
+			"city": "Peoria",
+			"state": "AZ",
+			"zipcode": "85383",
+			"country": "US",
+			"lat": 33.71171951,
+			"lon": -112.27405549,
+			"geohash": "",
+			"street_addr_2": "",
+			"latitude": 33.71171951,
+			"longitude": -112.27405549
+		},
+		"type": "restaurant",
+		"description": "",
+		"local_hours": {
+			"operational": {
+				"Monday": "Open 24 hours",
+				"Tuesday": "Open 24 hours",
+				"Wednesday": "Open 24 hours",
+				"Thursday": "Open 24 hours",
+				"Friday": "Open 24 hours",
+				"Saturday": "Open 24 hours",
+				"Sunday": "Open 24 hours"
+			},
+			"delivery": {
+				"Monday": "Open 24 hours",
+				"Tuesday": "Open 24 hours",
+				"Wednesday": "Open 24 hours",
+				"Thursday": "Open 24 hours",
+				"Friday": "Open 24 hours",
+				"Saturday": "Open 24 hours",
+				"Sunday": "Open 24 hours"
+			},
+			"pickup": {
+				"Monday": "Closed",
+				"Tuesday": "Closed",
+				"Wednesday": "Closed",
+				"Thursday": "Closed",
+				"Friday": "Closed",
+				"Saturday": "Closed",
+				"Sunday": "Closed"
+			},
+			"dine_in": {
+				"Monday": "Open 24 hours",
+				"Tuesday": "Open 24 hours",
+				"Wednesday": "Open 24 hours",
+				"Thursday": "Open 24 hours",
+				"Friday": "Open 24 hours",
+				"Saturday": "Open 24 hours",
+				"Sunday": "Open 24 hours"
+			}
+		},
+		"cuisines": [
+			"Breakfast",
+			"Vegetarian",
+			"Family Meals",
+			"Breakfast And Brunch",
+			"Chicken",
+			"Coffee & Tea",
+			"Hamburgers",
+			"Diner",
+			"Kids Menu",
+			"American",
+			"Dinner"
+		],
+		"food_photos": [
+			"https://cdn-img.mealme.ai/72f267fcef3347bb6ee7b7039012b74e36019970/68747470733a2f2f6d656469612d63646e2e677275626875622e636f6d2f696d6167652f75706c6f61642f746535716b6f7075677861786a79397a6674676d",
+			"https://cdn-img.mealme.ai/49d8d646d2d897398be7d3670965e15d9dba7c5e/68747470733a2f2f643172616c736f676e6a6e6733372e636c6f756466726f6e742e6e65742f64623132616530392d323436372d343261342d386665352d3062396637666433343638362e6a706567",
+			"https://cdn-img.mealme.ai/a1c2c85e527cb8275f0a293b28be089666b60e07/68747470733a2f2f643172616c736f676e6a6e6733372e636c6f756466726f6e742e6e65742f33663730366636632d643830642d343231622d623030392d6630326335333234363962332e6a706567",
+			"https://cdn-img.mealme.ai/deb947c3eb0211564f469054c426981dd8d38f76/68747470733a2f2f643172616c736f676e6a6e6733372e636c6f756466726f6e742e6e65742f35653136386234332d613666612d346462632d613238652d3536373033323362336564662e6a706567",
+			"https://cdn-img.mealme.ai/76a9edc69f20f1384dbf673ecac8340b5687edaa/68747470733a2f2f6d656469612d63646e2e677275626875622e636f6d2f696d6167652f75706c6f61642f756f70723277756c6e396d767161646666647432",
+			"https://cdn-img.mealme.ai/5ca58a7ce4b81b2352d670f39f502a30cddc2806/68747470733a2f2f643172616c736f676e6a6e6733372e636c6f756466726f6e742e6e65742f36393365633436662d363631622d343739662d626236312d3536323061393130396230352e6a706567",
+			"https://cdn-img.mealme.ai/823045325e51850ceb1dbed869ca97c5e10e8144/68747470733a2f2f643172616c736f676e6a6e6733372e636c6f756466726f6e742e6e65742f62613463663630362d323533662d343737662d386439642d3137613438643666653739332e6a706567",
+			"https://cdn-img.mealme.ai/ddfc33ca9950f56932ec142c694b94b361ea4246/68747470733a2f2f643172616c736f676e6a6e6733372e636c6f756466726f6e742e6e65742f63336235613738612d373061612d343164372d393561662d3337376632336432373239332e6a706567"
+		],
+		"logo_photos": [
+			"https://cdn-img.mealme.ai/5ca58a7ce4b81b2352d670f39f502a30cddc2806/68747470733a2f2f643172616c736f676e6a6e6733372e636c6f756466726f6e742e6e65742f36393365633436662d363631622d343739662d626236312d3536323061393130396230352e6a706567",
+			"https://cdn-img.mealme.ai/ddfc33ca9950f56932ec142c694b94b361ea4246/68747470733a2f2f643172616c736f676e6a6e6733372e636c6f756466726f6e742e6e65742f63336235613738612d373061612d343164372d393561662d3337376632336432373239332e6a706567",
+			"https://cdn-img.mealme.ai/a23af44fc09afe8b310e1cce19281dded6859a41/68747470733a2f2f6d656469612d63646e2e677275626875622e636f6d2f696d6167652f75706c6f61642f76313539343233383136312f716376766c7a6263726e79623879717a6c6361732e6a7067"
+		],
+		"store_photos": [
+			"https://cdn-img.mealme.ai/5ca58a7ce4b81b2352d670f39f502a30cddc2806/68747470733a2f2f643172616c736f676e6a6e6733372e636c6f756466726f6e742e6e65742f36393365633436662d363631622d343739662d626236312d3536323061393130396230352e6a706567"
+		],
+		"dollar_signs": 3,
+		"is_open": true,
+		"is_closed": false,
+		"offers_first_party_delivery": false,
+		"offers_third_party_delivery": false,
+		"miles": 3.0892889565182813,
+		"weighted_rating_value": 4.166274509803922,
+		"aggregated_rating_count": 255,
+		"_id": "42dc41b7-82bb-475d-a296-98adfab5899f",
+		"menu": {
+			"categories": [
+				{
+					"name": "Family Feasts (IHOP ‘N GO only)",
+					"menu_item_list": [
+						{
+							"name": "Breakfast Family Feast with Pancakes",
+							"price": 4360,
+							"qty_available": null,
+							"size": "",
+							"unit": "",
+							"description": "Four servings each of scrambled eggs and golden hash browns, 8 hickory-smoked bacon strips, 8 pork sausage links, and 8 fluffy buttermilk pancakes. Serves 4.  \r\n\r\nAvailable for IHOP ‘N Go only. Not available for dine-in.      ",
+							"image": "https://cdn-img.mealme.ai/b35d9b1aad98eb4d12fd9f3e7f48ddc1d2b45071/68747470733a2f2f74622d7374617469632e756265722e636f6d2f70726f642f696d6167652d70726f632f70726f6365737365645f696d616765732f65353665646138313165316365633934383262393331613831336163613464332f38353962616666316437363034326134356533313964316465383061656337612e6a706567",
+							"product_id": "gAAAAABijcmtqeXiG-peFGhQN3dTlfLV3j22dFn_7FTvnhEhxY9jluc5jaOKpYOXOhxKwN69bykJpNFZaZznRXfx6mgdZEPmcQrsGdzcuMpDnyd-cF_XffrD3sOKZWzB-cN7kS4DKrTQr0nJINEiJSF5Sz4tRxkoBTopUo52NdK_KG-YGfRhR64vBjUNJvhi-lRLIf8zjCtQ2zTtJJUupG6Zocd_-HxwGjeEVGvveOdoulmvj0Pm3Dh0NL2G91lgvKBScPHKKB_Egt29YyeBR5-XA-9YpvAZryrQPx3JRsgNgsPXBwHkEu8XxwiVg6V4YTrzwm75Ewuw91RTjaQidVDFumKdvg4HIE5N_skmkQwVJoJ5RVKlSt1PL5bL8SaaFv05Am44Vc2GjAH7MWJrmHGhvjxyHnf1VNxV8Mkokfwow0pqp6hkzn6qCmRRandRMxZF-9YQAQuFEmhUvFl_QwjdcO5ndWoCRiAqpV93kGbneNunvlgb2oRUqTtkB-7_2I06LZAhD5-g9VvrqxqjRkCbhOIRvcBpcuKYiAr63oEPaHx53z-Vy_mr1nHkJhi2mfszH24-1gOBIITPg2FN9PesYd9gWBRVoq82R3F96dSM4_xENoZVY9ZSntcwGnAwz_R1WGXqmbZOL_TgoFN24tvCn2lAjJXnuEs9Pkq38ZMCuw0BtKhvXpjgofhj0n_bzks7qOqRSeQx",
+							"should_fetch_customizations": true
+						},
+						{
+							"name": "Breakfast Family Feast with Waffles",
+							"price": 4360,
+							"qty_available": null,
+							"size": "",
+							"unit": "",
+							"description": "Four servings each of scrambled eggs and golden hash browns, 8 hickory-smoked bacon strips, 8 pork sausage links, and 12 Belgian Waffle triangles. Serves 4.  \r\n\r\nAvailable for IHOP ‘N GO only. Not available for dine-in.      ",
+							"image": "https://cdn-img.mealme.ai/7d39e2dba35f7788125040f942140b41f064fe9e/68747470733a2f2f74622d7374617469632e756265722e636f6d2f70726f642f696d6167652d70726f632f70726f6365737365645f696d616765732f66626130376563333830386231313339316434376562393537613338383661652f38353962616666316437363034326134356533313964316465383061656337612e6a706567",
+							"product_id": "gAAAAABijcmt_PPNUzxNWozg6HXpv16V9l_d_iPRP0s7rQRjXg773rmYsh0CNTbnLZkOZwLYuO8t1wiQ2qIwM6WeKvAVWXjdcTvQ2NfnJGuz5Dfq2poLNURTmes2v5kT-TbTQJM5DnoiLlYiCEfQca_qIaWBW4e1NRDshe85hp6bFc76t_PyKe2tNXgtM4iYruZw6JEPpVKhpUJp9dBAkEM6559UGZtZkeSR2hYm4XNxsj0BTeVIVvThuCzm2v7uU3HJaxSF_f6bnpETSj-fuoSPJGpk8r8Za-KF9jm7l76YnY6XPk0VJPrnFA8Udd57sFDzgzCeptexAJvgiB1q4SXdDfttIjbjM7y3QxMU_n6AxVDgsCV10FI2sLKzwtZH7j-Ipa8yRkAwpCv8CQNIj-cfhJ9lEsDGRAB4eXaM71Zgsmpq3plvbMsGHp58aBm6hzpfsxlK7Gsffs7cVx29kA7fNXnDsPQHjQTtoDUV_UkBrWEQaQNNaAYHiR-SrpOUwkxlqBUsMC1KMPkNol3WBp2K7UKSKdcKZCLdUX0S-S31PB_pLp2TUTrcaUGuMTDfR3zT8chjKduLFrO5ddM5e-KwHnDTaX8d559obAb0UvAJR2-G6TrozprEZzU7KxPoYAroTDTMaQvZ2cQDPgSHhioNYOU7twSHS0hqsY8RT5ak2Q404Ihd-xwkxR2AUIP7FxNyTQd7mPmY",
+							"should_fetch_customizations": true
+						},
+						{
+							"name": "Breakfast Family Feast with French Toast",
+							"price": 4360,
+							"qty_available": null,
+							"size": "",
+							"unit": "",
+							"description": "Four servings each of scrambled eggs and golden hash browns, 8 hickory-smoked bacon strips, 8 pork sausage links, and 8 triangles of French toast.  Serves 4.  \r\n\r\nAvailable for IHOP ‘N GO only. Not available for dine-in.      ",
+							"image": "https://cdn-img.mealme.ai/0f79094c24e2c51f95ab05026627d06a03c3ba2c/68747470733a2f2f74622d7374617469632e756265722e636f6d2f70726f642f696d6167652d70726f632f70726f6365737365645f696d616765732f34386130343534343438616166663938643035373465303463653239376666302f38353962616666316437363034326134356533313964316465383061656337612e6a706567",
+							"product_id": "gAAAAABijcmtG1jTf0gww6qyPxulharK-5Q_8t7d-Ii6fHqJpg3cEKSBbxLCoMscGXRrP1gUYVYWZVpzDhCrEsrIzV-qmc-ZvCZARFW1FMlZHrMFfzt542JV1PXyfoGrjpctr3OQ3cVR7aOF9K_Z4diFmDHnXJ2U1xZvxAJN5UhXQ8dtrQkV9x2TOA0yWgT2SYhCNFLzIeWxQUX_ohVd5FiuRf4JzHLmSl3EV3GlBQC-h193xWKEecd2S_I5RQet0qsD61azFDY0f9V02tDxZ8oSOZCXE1gajX4rAgEqpIXyuDAzUNKK-FMO0dIQyUnySs1Ty26UP9hexe3qgI9QARALSxe2R9dY99erto7m0491x6SmJ7LtNqpLTHK58fyuzhxkaE-kzUmWRSSj4WjXL_hKeedpsodF177BeoGTXZXewcag6LCYVdgbDXNyoSyaQa_DasAUq3iERrT4XDwMnAX2lfClH4BNBaCUaLdorH2LPcDnKAnfhSI1xi6rkHAapFQrQgrrUKznek1I03W-XeoemRf2Vs5DNi4NHaBsOphL-Eu6iXBob2jTD2a6-g92CNiRp2MLUSOefavdEb6V57I-GBjE04nLqgKRq3oDwX7iQA0f37lNlkFqibq7jY4ecnxPmoi3eR-5E5WFJqhfj5xSXefFQ9npkt9Hnvhq7BDIo2Rt3mj6mFj2WnY8vLpFQXE73tmrWIvd",
+							"should_fetch_customizations": true
+						},
+						{
+							"name": "Pancake Creations Family Feast with Bacon",
+							"price": 4360,
+							"qty_available": null,
+							"size": "",
+							"unit": "",
+							"description": "8 of our world famous buttermilk pancakes, 4 servings each of scrambled eggs and golden hash browns, 8 hickory-smoked bacon strips, and choice of 2 pancake toppings. Serves 4. \r\n\r\n Available for IHOP ‘N GO only.  Not available for dine-in. ",
+							"image": "https://cdn-img.mealme.ai/37c5fb3fe9c9f0312c4cc287f2acf4455a222782/68747470733a2f2f74622d7374617469632e756265722e636f6d2f70726f642f696d6167652d70726f632f70726f6365737365645f696d616765732f35656136656639656437396437353435383932643666653964623463306636632f38353962616666316437363034326134356533313964316465383061656337612e6a706567",
+							"product_id": "gAAAAABijcmtg81_RSmgoBBXbz_HeY3j4UvkuNmlBz_LkgYtVO6tQm7zYV6pEKJnyogdT1P-IxCpa9ws7tNcUuJKzmqGmNKfX1E1Gz7TBaTOh04uFxLx1_eiOULuT_jwW6LAuoLlZh2XyAKEKnpzNoULoDOyvTZ7VIDgjXsbKEuQdj7goKYCL848XvO49McozcK-lsoG1wB-I1pCEqjnT1AR4i1-ihbHD3124SSPUp_pUtC5Q3Gx2EVo9zcj4uM4mBzZwc1j9yDcQ0LG11Ry-fjhYyHttJ7IRe7d9xTrsZSYUlNaUwF_Flk45Ypni95lSojoozvxIFo90WUj_W72U-xMN6T4j5Ox65wRuPGOKevQDqiLgmAGGqa5dTabcA1DUxv3zAu0LnhXko7oIOOe6CRLiL9r-qo9g-TPl9usuoZmRFV8wUG9QNSKocco5QI5dFXrUEX8vOE8eUelTz8kkL5wHJVItHWubKvyZt9doVzgLUNlftTFMxJw_Df8ebUJLyNzzHDcYQRExUKKS-xt8RP3pe_LK49i_UNi2PXkbYLWQG0cJK0_R3X3d4Q07G1mcnV6AMuRvqhLcXPddEPabjG0VRpbDnMjDUclfkCbtPMmKCcLr3HiD2WESwCdgN8HDXx40llxSj7ecSniisipgjzGyzBOWgw_M95ymeHsJ7EFs6OWD-TDQHqiXPn2tqZRhA50t2MWx4uS",
+							"should_fetch_customizations": true
+						},
+						{
+							"name": "Pancake Creations Family Feast with Sausage",
+							"price": 4360,
+							"qty_available": null,
+							"size": "",
+							"unit": "",
+							"description": "8 of our world famous buttermilk pancakes, 4 servings each of scrambled eggs and golden hash browns, 8 pork sausage links, and choice of 2 pancake toppings. Serves 4.  \r\n\r\nAvailable for IHOP ‘N GO only.  Not available for dine-in.",
+							"image": "https://cdn-img.mealme.ai/23329db7d08fb18144ef8bc950b355ed41da9772/68747470733a2f2f74622d7374617469632e756265722e636f6d2f70726f642f696d6167652d70726f632f70726f6365737365645f696d616765732f66316534363662366261376565383533386531336437336132663433346434652f38353962616666316437363034326134356533313964316465383061656337612e6a706567",
+							"product_id": "gAAAAABijcmtAIfGyXZmGCagg6frr-FwstMz99LmjexfXcT8Wuv1eH36aQfrkngd86Ccm_TjDCky5yiYDkzMsLTvJyUyOvreiqSNa3pdKecGoWkNlFZjS6HGULAOz6bvg7bGAa8UB-CnXgXwt3hBQgQZxaQnlWFxRHvYp1WSOgAoGB-S5rJsIwT0iDGrR0hKkWS8MqONOcjagewCbEBi6cZWG2rRkN-eMzjtmbYHm6yGiK9PmpowqgsRyiITH0lHJMZ8AbeRgpzic14u2T9txj627jbin9lQKAvC3rKyEN7rChaCodHk1WpJI6UgGtp4yHuwsf1w11hgRJ3UxjVtUJi7AHBYKAQ7Twy2Lf7L836KZkiTMcbny_vhDWrNpbmi7Z_DVeBs7oI3dGP_IhQ4ZkXFeC4K9npnhTYS0kTL-_WbqY9hsnJpiTcE3AVioMGD8RUcTFzuZM-jg0bi7xUnB034tXoGAneN1Ehd3_llfE_ajHGmuS_PL_TjKpkTBGvrS9oWd5KfXk26qtD-p6gO4K4s_M6IrZDQONxSe4lBC3Zu8UA9QleIu9d3If8tm88UY9EHWscbdoELhUVw-k8wOzJCLem15-SHGBl67FldrFVLEhOgCInqDZu_QQqzZwkipiohRll90mMbVP7NZYXuc5k0--LP7lsOvmnUIsgsIgclKGcp7MFsgY5whaJQp-sdrpTMoqoOVF2h",
+							"should_fetch_customizations": true
+						},
+						{
+							"name": "Steakburgers & Chicken Sandwiches Family Feast",
+							"price": 4620,
+							"qty_available": null,
+							"size": "",
+							"unit": "",
+							"description": "Choice of 4 Ultimate Steakburgers, 4 Buttermilk Crispy Chicken Sandwiches, or 2 Ultimate Steakburgers and 2 Buttermilk Crispy Chicken Sandwiches.  Served with a basket of French Fries and lettuce, tomato, red onion, pickles, and IHOP® sauce on the side.  Serves 4.  \r\n\r\nAvailable for IHOP ‘N GO only. Not available for dine-in.      ",
+							"image": "https://cdn-img.mealme.ai/995c5fc5bf42babb55537ba243c4e84ec7f854a5/68747470733a2f2f74622d7374617469632e756265722e636f6d2f70726f642f696d6167652d70726f632f70726f6365737365645f696d616765732f38623134346632333364313437313735363266346662343938313934383131612f38353962616666316437363034326134356533313964316465383061656337612e6a706567",
+							"product_id": "gAAAAABijcmtAYyapidQvydx80XMycy7sGYacEChvlTtdAUMoA75sq5Oc-pFIYqaOxXT6ZAIRFRSVDixCYbUGtwbyTEbWVQnPt5uM7wrF4eNJH8Vt4cqkoz2PsRFGut421rBVlCqVhDpOu9LhP3wPxy_-CRnaZUqhscewzCcFq7iNTB-J_93QvT_UUt-n6btUol1kKR4OsCbmaMUKtBx-VGsU7oDwVqKo4ZRR6rGh_DC96eMdfiflvL5dwL7V6m2WpRTL41qbPpDm4aTLzZGvo32BguG8Npdxo2Yd9Q4t8N5l7OlKFzobi24oICzBEa2150XY-DTYlD38UARaQ9WOVZBOWtaNCUHsn8DHW__6mdKi61zH-bI-rXX0KpQDlKX4bf8gVZ5EW9dM4cqcv9bEcgIESb9L2gSCV_KaX2tepuas6Qvy0QkpCUhYHt-XvtaCX2vTXszuNxGqyu-C0zULAa6hTqkskqcplfyWvtaBDRimV6PwMmFUAuqZVYQFVx6b2Uk3QAeRKnWIGTp2XczByh0OMMLR78pFsb_YmHenU63DZobwoe0K573Jjm33tuqTYZu9rXaJCoMua_X_fuggWSucPqeAob1IdPcJfALPlGTzVi-QNqJzw6apAtNUE2NJVjf-373dNpmdB48CNJDgOrO-pf5pfLd_Eu5EIfPmXNj1oY6g5-cGL9bxCb2xDoPBmdCk3zfmvYn",
+							"should_fetch_customizations": true
+						},
+						{
+							"name": "Breakfast Beverage Bundle",
+							"price": 1320,
+							"qty_available": null,
+							"size": "",
+							"unit": "",
+							"description": "Choice of 4 16oz. beverages.  Choose from Regular or Decaf coffee, or for an additional charge, choose Orange Juice, Apple Juice, Milk or Chocolate Milk.\r\n\r\nAvailable for IHOP ‘N GO only. Not available for dine-in.     ",
+							"image": "https://cdn-img.mealme.ai/94f0ad149381ca23725d1a24ba8d1d14a5b6ea5f/68747470733a2f2f74622d7374617469632e756265722e636f6d2f70726f642f696d6167652d70726f632f70726f6365737365645f696d616765732f31366332313634343533306538333534346636323838313262326662666436312f38353962616666316437363034326134356533313964316465383061656337612e6a706567",
+							"product_id": "gAAAAABijcmtNaz_Ei0Z3PZFleYLTKXWVU-xnpvNMpqW2DbMxGb5XYjHteqRVl0G3SJmjJWUodcONr98258UVsT0TOWwGkWk_WMAESXWcEmAEzi7C0bSJxvf_vGjQxtU86OuCjmMiKdK2tN6E3cJ4u_ZHL3AEXZrnA3yYtzfm6cXRVs7A_SNT1bpkahttFgdMBcOc4FoRag0-WLIaKlAQDfFd4_w2l6Yz_eGZQsIhXLH54i-UT_StXSEFnLMcZcO0OGFQjZbMYUG0dgd6Q8HMAzbZjSJV6TbCjTVK3vMbIK89EchJzpSwbwviu3oQNFWUgIWg449LS-UvfVHWKy0WOXP6mfgFqAtXTNuBbwY3xDrdx-5sI0G0VszhczcJADm3c1XhHVd4gNFIyKCX0MFMQnFhu7b3Ayh3U3YaVi0IHzUMR_b5abOdLdzcFPfy-CGLyZ3Pm5BgNg16FsecRbYBOVLw7OV0ZXBuiKu3qEOjvmn3zspWVVmGh3YTrpdD38fmgLCpjYks2AkTYQ7JD90b2E8BXKMqC0v7Ttug4VaihOBol9Slcus1ZH0Fubp6tlbsKBwAnlr4cUgVUc3cQnRs_4ktUm4sxlXePJhIuV5WK-iqm-ZsKXSxmIJNczHUXYGv-d6KKMlZ-ICQURbcdrwTG99Tk0HlRelssqJ9wItfPzdmLmdICRBHIIcAXyB6NKsBQCG9ZY-ieK2",
+							"should_fetch_customizations": true
+						},
+						{
+							"name": "Lunch/Dinner Beverage Bundle",
+							"price": 1460,
+							"qty_available": null,
+							"size": "",
+							"unit": "",
+							"description": "Choice of 4 refreshing 30oz. beverages.  Choose from Fountain Drink Sodas, Lemonade or Iced Tea.  \r\n\r\nAvailable for IHOP ‘N GO only. Not available for dine-in.     \r\n",
+							"image": "https://cdn-img.mealme.ai/ac873801e81644c3ac7ec147a10330ff78719f40/68747470733a2f2f74622d7374617469632e756265722e636f6d2f70726f642f696d6167652d70726f632f70726f6365737365645f696d616765732f37323261636239356331333836313931633466376164346432376666643730612f38353962616666316437363034326134356533313964316465383061656337612e6a706567",
+							"product_id": "gAAAAABijcmtkTCtdR0BkucdWKxQ0oo65loUFtMUWa35addMdS4WaVMN6Qk0WtRkc7Kg406s3cDYiGUxqBKs6ND2uQxjz9lDmILRXJDs7fbKtghr505XbOKRMrvYmlxG-rBfExwXSuWwiBL-YTQNJ8zgPOaIHQZ-OuLCoU6lFjavF_s8ALquUd1Bsp6LOPtd5BJXAcEs_6wg6ZEBrDS546DlZzS6cvMGfTOftyrPQDz22OA_ogfOSqdW1tPFeXeKtiMJrEZzNExEK3gJkk8rF7IcFB3-SJMxeqXr8uGj1wmNJUwMkDTeF1PjgJoVVVW45-7wSId633BZ4GSh_MTcOVg8zJARwH9g_4Btf5n79nm-6rJ9CpDKpe1U_hrtWdEtS1jYhHtrPLj9v-jPGa5SGXR5-G7uec65kMiCuNZ40A057I1CbhPunqcfcgtWbdVSPbAj0M153bkOmUnfBetMslWxAkgW694UyhiYh2bj8qm9WeL6U8a9-KWsnQdTdbhrDkBWI8uyRn9oTFVngZG7NkxCkrwQEqisCHbHa_pprNtW62oB5NFRpZ0J2KDAC3HMrh97GVI7ocMoRquSC3yOoGWJOri1maZ0x3jvboRo1X9QZYluZeR2AjNwDkSiBpAiV6qDNWEgsunb4oLuuSym5V-DttlON0yFVrwyDFlM6t2zZSRxxz8oFXohTmwJpepVXtIS69hwqA0W",
+							"should_fetch_customizations": true
+						}
+					]
+
+				}
+			]
+		}
+	}
