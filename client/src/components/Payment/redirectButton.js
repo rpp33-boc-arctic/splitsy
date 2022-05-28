125262a30cddecc6f54a8c0528a7ee29a4910a25
yfcheng1 <b99501048@gmail.com>
2022-05-27 22:39:54 -0400

    add link to page

--- client/src/components/Payment/redirectButton.js
+++ client/src/components/Payment/redirectButton.js
@@ -3,12 +3,15 @@ import Button from '@mui/material/Button';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
 import Stack from '@mui/material/Stack';
+import { Link }  from "react-router-dom";
 
 const RedirectButton = (props) => {
   return (
     <Stack direction="row" spacing={2}>
       <Button variant="contained" endIcon={<ShoppingCartIcon />}>
-        BACK TO CART
+        <Link to="/Cart" style={{'textDecoration': 'none', color: 'white'}}>
+          BACK TO CART
+        </Link>
       </Button>
       <Button variant="contained" endIcon={<ArrowCircleRightIcon />}>
         SUBMIT ORDER
