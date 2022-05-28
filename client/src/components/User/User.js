1ad7e93e6c07ab54de10f2e35fc1866a1b5cd882
Dennis Wang <denniswang.self@gmail.com>
2022-05-28 10:01:04 -0700

    Added navbar to user profile

--- client/src/components/User/User.js
+++ client/src/components/User/User.js
@@ -7,8 +7,9 @@ import Button from '@mui/material/Button';
 import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
 import QrCode from '@mui/icons-material/QrCode';
 import Settings from '@mui/icons-material/Settings';
-import Item from './Item.js';
-import Friend from './Friend.js';
+import Item from './item.js';
+import Friend from './friend.js';
+import NavBar from './navbar.js';
 
 class User extends React.Component {
   constructor(props) {
@@ -26,44 +27,47 @@ class User extends React.Component {
       return <Friend friend={friend} key={i} />
     })
     return (
-      <Grid container spacing={1} id="payment-page">
-        <Grid item xs={1}>
-          <img src={userData.results[1].photo_url} alt="userPhoto" width="100"></img>
+      <div>
+        <NavBar /> <br></br>
+        <Grid container spacing={1} id="payment-page">
+          <Grid item xs={1}>
+            <img src={userData.results[1].photo_url} alt="userPhoto" width="100"></img>
+          </Grid>
+          <Grid item xs={11}>
+            @exampleUsername
+          </Grid>
+          <Grid item xs={2}>
+            "Got paid today, time for some extra guac on my Chipotle!" <br></br> <br></br>
+          </Grid>
+          <Grid item xs={6}>
+            Order History
+            <List >
+              {items}
+            </List>
+          </Grid>
+          <Grid item xs={4}>
+            Friends List
+            <List >
+              {friends}
+            </List>
+          </Grid>
+          <Grid item xs={12} container justifyContent="flex-start">
+            <Button variant="contained" endIcon={<Settings />}>
+              Setting
+            </Button>
+          </Grid>
+          <Grid item xs={12} container justifyContent="flex-start">
+            <Button variant="contained" endIcon={<QrCode />}>
+              QR Code
+            </Button>
+          </Grid>
+          <Grid item xs={12} container justifyContent="flex-start">
+            <Button variant="contained" endIcon={<ArrowCircleRightIcon />}>
+              Log Out
+            </Button>
+          </Grid>
         </Grid>
-        <Grid item xs={11}>
-          @exampleUsername
-        </Grid>
-        <Grid item xs={2}>
-          "Got paid today, time for some extra guac on my Chipotle!" <br></br> <br></br>
-        </Grid>
-        <Grid item xs={6}>
-          Order History
-          <List >
-            {items}
-          </List>
-        </Grid>
-        <Grid item xs={4}>
-          Friends List
-          <List >
-            {friends}
-          </List>
-        </Grid>
-        <Grid item xs={12} container justifyContent="flex-start">
-          <Button variant="contained" endIcon={<Settings />}>
-            Setting
-          </Button>
-        </Grid>
-        <Grid item xs={12} container justifyContent="flex-start">
-          <Button variant="contained" endIcon={<QrCode />}>
-            QR Code
-          </Button>
-        </Grid>
-        <Grid item xs={12} container justifyContent="flex-start">
-          <Button variant="contained" endIcon={<ArrowCircleRightIcon />}>
-            Log Out
-          </Button>
-        </Grid>
-      </Grid>
+      </div>
     )
   }
 }
