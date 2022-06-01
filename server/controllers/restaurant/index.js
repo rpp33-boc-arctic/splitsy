954f7f8be2ce1c567ee1a31b16919eb4f1a47e49
Grant Mitchell <soundbreakr1@gmail.com>
2022-05-31 17:56:07 -0700

    override

--- server/controllers/restaurant/index.js
+++ server/controllers/restaurant/index.js
@@ -6,17 +6,22 @@ module.exports = {
   restaurantList: (req, res) => {
 
     console.log(req.query);
-    if (req.query.add){
+    if (req.query){
+// 33.69292', long: '-112.3228
 
-      if (req.query.lat >  33.695  && req.query.lat < 33.69733 ){
-        if (req.query.lon >  -112.317814  && req.query.lon < -112.3180 ){
+      if (req.query.lat >  33.692  && req.query.lat < 33.695 ){
+        if (req.query.long  <  -112.320  && req.query.long  > -112.325 ){
           console.log('in range of address');
           fs.readFile(path.join(__dirname,'../../../DATA.json'),'utf8',(err,data)=>{
             console.log(err,data);
-            res.send(data);
+            res.send(JSON.stringify(data));
           })
 
+        } else {
+          res.send('no data')
         }
+      }else {
+        res.send('no data')
       }
 
 
