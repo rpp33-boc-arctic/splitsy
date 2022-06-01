7b53caf4042b0cb9ec38de611aac10bce9dd9018
Nachcha Aroonrerk (Neen) <neen.nachcha@gmail.com>
2022-06-02 00:42:34 +0700

    Working on main board 1. complete initial state in payment.js 2. complete main skeleton in payment.js & item.js / 3. complete function updateItemsOnMainBoard which updates GET users cart_item. Working on componentDidMount in payment.js

--- client/src/components/Payment/Payment.js
+++ client/src/components/Payment/Payment.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+
 import Timer from './timer.js';
 import UserList from './userList.js';
 import ItemPaidBar from './itemPaidBar.js';
@@ -18,10 +20,77 @@ class Payment extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+      user_pick: [],      //array of order_item_id of current user
+      username: "",       //from cookie (after session_cookie had verified)
+      user_id:"",
+      group_cart: {},
+      session_id: 1,      //session id from cookie? or pass from other component
+      others_pick: [],    //array of order_item_id of others
+      not_yet_pick: []    //array of order_item_id of non chosen item
     }
   }
 
+  //==========================     MAIN     ==========================
+  componentDidMount () {
+    this.updateItemsOnMainBoard();
+  }
+
+  handleAddToCart () {
+
+  }
+
+  handleRemoveFromCart () {
+
+  }
+
+  addAllNonPickedToCart () {
+
+  }
+
+  //==========================     HELPER     ==========================
+  updateItemsOnMainBoard () {
+    axios.get(`/session${this.state.session_id}/users`)
+      .then((users) => {
+
+        users = users.data[0].users
+        // console.log('users', users)
+        var all_item = Object.keys(this.state.group_cart);
+        var others_item = [];
+        var my_item = [];
+        var all_chosen_item = [];
+        var non_chosen_item = [];
+
+        for (var userId in users) {
+          if (userId !== this.state.user_id) {
+            others_item =  new Set([...others_item, ...users[userId].user_cart])
+          }
+
+          if (userId ===  this.state.user_id) {
+            my_item = [...my_item, users[userId].user_cart]
+          }
+        }
+
+        all_chosen_item = [...my_item, ...others_item];
+        non_chosen_item = new Set([...all_item, ...all_chosen_item]);
+
+
+        this.setState({
+          user_pick: my_item,
+          others_pick: others_item,
+          not_yet_pick: non_chosen_item
+        }, () => {
+          // console.log('state', this.state);
+        })
+      })
+      .catch((err) => {
+        console.log('error in handleItemPick', err)
+      })
+  }
+
+  getPrice (order_item_id) {
+
+  }
+  //==========================     RENDER     ==========================
   render() {
     return (
       <Grid container spacing={1} id="payment-page">
