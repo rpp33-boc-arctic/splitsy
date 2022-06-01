26912bdc81bb5e11f779b37a733227affa8e8d2e
Grant Mitchell <soundbreakr1@gmail.com>
2022-05-31 17:54:44 -0700

    added code

--- client/src/components/rest/rest_pick/requests.js
+++ client/src/components/rest/rest_pick/requests.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 function getRestaurants(lat,long,miles){
-  axios.get('/restaurant',{params:{lat:lat,long:long}});
+  axios.get('http://127.0.0.1:3001/restaurant',{params:{lat:lat,long:long}});
   //grab data thats x miles away         "miles": 3.0431106214340744,
 
 }
