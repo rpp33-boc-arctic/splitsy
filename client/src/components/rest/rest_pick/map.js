0a5257661cc43736235e50179a329d3dc409abdb
Grant Mitchell <soundbreakr1@gmail.com>
2022-06-02 12:49:08 -0700

    fixed back to old way

--- client/src/components/rest/rest_pick/map.js
+++ client/src/components/rest/rest_pick/map.js
@@ -12,8 +12,10 @@ class Map extends React.Component {
       this.state = {
         query:"",
         error:false,
-        helperText:"pleaes enter a valid address."
-      }
+        keywords:"",
+        helperText:"pleaes enter a valid address.",
+        join:""
+        }
 
   }
 
@@ -35,26 +37,39 @@ class Map extends React.Component {
   this.search = this.search.bind(this)
   }
 
-  querychange(e){
+  querychange(e,name){
     // this.query = e.target.value;
-    if (e.target.value === ("12145 west jessie ct") || e.target.value === "1802 Notre Dame Ave, Belmont" ){
-      this.setState({query:e.target.value,error:false,helperText:"congrats it valid"})
-    } else {
-      this.setState({query:e.target.value, helperText:"pleaes enter a valid address."})
-    }
+      if (name === "query"){
+        if (e.target.value.length > 10 ){
+          this.setState({query:e.target.value,error:false,helperText:"congrats it valid"})
+        } else {
+          this.setState({query:e.target.value, helperText:"pleaes enter a valid address."})
+        }
+      }
+
+      if (name === "keywords"){
+          this.setState({keywords:e.target.value})
+      }
+      if (name === "join"){
+        this.setState({join:e.target.value})
+       }
+
   }
   search(e){
-    if (this.state.query.toLowerCase() === '12145 west jessie ct' || this.state.query === "1802 Notre Dame Ave, Belmont"){
+
+
+    if (this.state.join.length >= 4){
+      //joining session;
+    } else {
     var url = `https://api.tomtom.com/search/2/search/${this.state.query}.{json}?key=${MapAPI}`;
     setTimeout(()=>{
       axios.get(url).then((data)=>{
-        console.log(data.data.results[0].position);
+        // console.log(data.data.results[0].position);
+        //mark map position on serach
         this.props.getRestaurants(data.data.results[0].position['lat'],data.data.results[0].position['lon'])
       });
       this.setState({query:"",error:false})
     },2000);
-  } else {
-    this.setState({helperText:"Restaurants do not exist out in the middle of nowhere!  ",error:true})
   }
   }
 
@@ -62,23 +77,23 @@ class Map extends React.Component {
   render() {
     return (
       <div className="map-ct">
-
         <div className="map">
     <div className="mapct">
       <span>{this.state.query } <br/> </span>
       <span>{this.state.error}</span>
-      <div className="search-ct"><TextField error={this.state.error? this.state.error: false}  helperText={this.state.helperText} style={{margin:"10px"}} onChange={(e)=>{this.querychange(e)}} id="outlined-basic" label="" variant="outlined" value={this.state.query} className="mapsearch" />
+      <div className="search-ct">
+
+        <TextField  error={this.state.error? this.state.error: false}  helperText={this.state.helperText} style={{margin:"10px"}} onChange={(e)=>{this.querychange(e,"query")}} id="outlined-basic" label="address" variant="outlined" value={this.state.query} className="mapsearch" />
+        <TextField error={this.state.error? this.state.error: false}  helperText={this.state.helperText} style={{margin:"10px"}} onChange={(e)=>{this.querychange(e,"keywords")}} id="outlined-basic" label="keywords" variant="outlined" value={this.state.keyword} className="mapsearch" />
+        OR
+        <TextField  helperText={"enter join code"} style={{margin:"10px"}}   onChange={(e)=>{this.querychange(e,"join")}} id="outlined-basic" label="join code" variant="outlined" value={this.state.join} className="mapsearch" />
+
       <Button onClick={(e)=>{this.search(e)}} className="submit"> submit</Button>
       </div>
       <div id="Map" className="Map" >
     </div>
     </div>
-
-
         </div>
-
-
-
       </div >
     )
   }
--- client/src/components/rest/rest_pick/rest_pick.js
+++ client/src/components/rest/rest_pick/rest_pick.js
@@ -12,7 +12,7 @@ class RestaurantPick extends React.Component {
       rest:[]
     }
     this.getRestaurants = this.getRestaurants.bind(this);
-    this.getPlaces = this.getPlaces.bind(this)
+    // this.getPlaces = this.getPlaces.bind(this)
   }
 
   componentDidMount(){
@@ -27,44 +27,16 @@ class RestaurantPick extends React.Component {
      }
 
 
-     getPlaces(address, query, distance){
-      axios.get('http://127.0.0.1:3001/getAddress',{params:{address:"12145 west jessie"}} ).then(data=>{
-      ;
-        //[0].formatted_address
-        var newdata = data.data;
-        console.log(newdata)
-        var returnAddress = newdata[0].formatted_address;
-        var latlon = newdata[0].geometry.location;
-        axios.get('http://127.0.0.1:3001/placesNearby',{params:{location:latlon,address:returnAddress,distance:distance}} ).then(response=>{
-          console.log(response)
-          this.setState({rest:response.data.results});
-          //[0].formatted_address
-
-        });
-      });
 
 
-
-
-
-      // axios.get('https://maps.googleapis.com/maps/api/place/photo',{params:{maxwidth:500,photo_reference:long}}).then(data=>{
-      //   console.log(data)
-      //   this.setState({rest:data.data})
-      // });
-      // https://maps.googleapis.com/maps/api/place/photo
-      // ?maxwidth=400
-      // &photo_reference=Aap_uEA7vb0DDYVJWEaX3O-AtYp77AaswQKSGtDaimt3gt7QCNpdjp1BkdM6acJ96xTec3tsV_ZJNL_JP-lqsVxydG3nh739RE_hepOOL05tfJh2_ranjMadb3VoBYFvF0ma6S24qZ6QJUuV6sSRrhCskSBP5C1myCzsebztMfGvm7ij3gZT
-      // &key=YOUR_API_KEY
-     }
 
   render() {
     return (
       <div className="rest">
       <p> Restaurants near me </p>
-      code:<input className="code"></input>
       <div className="container">
        <List rest={this.state.rest}></List>
-      <Map getRestaurants={this.getPlaces}></Map>
+      <Map getRestaurants={this.getRestaurants}></Map>
         </div>
 
 
