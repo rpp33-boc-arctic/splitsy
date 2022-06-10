b9afdcb71b643db1a983c051d08a0fb875e1b682
Nachcha Aroonrerk (Neen) <neen.nachcha@gmail.com>
2022-06-10 22:38:17 +0700

    fix nav bar, left and right, able to logout, but didn't delete broswer cookie

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,94 +1,110 @@
 import React from 'react';
 import axios from 'axios';
 
-import {Button, Typography, Toolbar, Box, AppBar, ListItem, List, Divider, ListItemText, IconButton} from '@mui/material';
+import {Button, Typography, Toolbar, Box, AppBar, IconButton} from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 
-import {Link, Outlet } from 'react-router-dom';
+import {Outlet } from 'react-router-dom';
+import DisplayMenuRight from './DisplayMenuRight.js';
+import DisplayMenuLeft from './DisplayMenuLeft.js';
 
 class NavBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      openNav: false,
-      isAuthenticated: false
+      // openNav: false,
+      // isAuthenticated: false,
+      openNavRight: null,
+      openNavLeft: null
     };
 
-    this.displayNav = this.displayNav.bind(this);
-    this.openNav = this.openNav.bind(this);
-    this.checkLoginStatus = this.checkLoginStatus.bind(this);
     this.handleLogout = this.handleLogout.bind(this);
+    this.handleOpenNavRight = this.handleOpenNavRight.bind(this);
+    this.handleCloseNavRight = this.handleCloseNavRight.bind(this);
+    this.handleOpenNavLeft = this.handleOpenNavLeft.bind(this);
+    this.handleCloseNavLeft = this.handleCloseNavLeft.bind(this);
   }
 
-  componentDidUpdate(prevProps) {
-    if (prevProps.cookieData !== this.props.cookieData) {
-      this.checkLoginStatus();
-    }
-  }
+  // componentDidUpdate(prevProps) {
+  //   if (prevProps.cookieData !== this.props.cookieData ) {
+  //     // this.checkLoginStatus();
+  //   }
+  // }
 
-  componentDidMount() {
-    this.checkLoginStatus();
-  }
+  // componentDidMount() {
+  //   // this.checkLoginStatus();
+  // }
 
-  checkLoginStatus() {
+  // checkLoginStatus() {
 
-    if (this.props.cookieData) {
-      const { username, userId } = this.props.cookieData;
-      console.log('cookieData is :', this.props.cookieData);
+  //   if (this.props.cookieData) {
+  //     const { username, userId } = this.props.cookieData;
+  //     console.log('cookieData is :', this.props.cookieData);
 
-      if (username && userId) {
-        this.setState({
-          isAuthenticated: true
-        });
-      }
+  //     if (username && userId) {
+  //       this.setState({
+  //         isAuthenticated: true
+  //       });
+  //     }
 
-    } else {
-      this.setState({
-        isAuthenticated: false
-      });
-    }
-  }
+  //   } else {
+  //     this.setState({
+  //       isAuthenticated: false
+  //     });
+  //   }
+  // }
 
   handleLogout() {
     axios.get('/logout')
       .then(() => {
         this.props.verifyUser();
+        // this doesn't delete cookie on the broswer yet.
       });
   }
 
-  displayNav(){
-    const style = {
-      width: '100%',
-      position:"relative",
-      // bgcolor: 'background.paper',
-    };
-   return ( <List  sx={style}  aria-label="mailbox folders" style={{left:'0px', position:'relative',top:'0', width:'400px', height:"auto", background:'#1976d2'}}>
-    {this.state.isAuthenticated? <ListItem button component={Link}  to='/' style={{marginLeft:"0px", height:"50px",color:"white"}} primary="profile" > <ListItemText primary="Profile" /> </ListItem>: null}
-    <Divider />
+  // openNav(){
+  //   this.setState({openNav:!this.state.openNav})
+  // }  detectOffClick(e){
+  //   var b1 = e.target.classList.contains('MuiTypography-root');
+  //   var b2 = e.target.classList.contains('MuiList-root');
+  //   var b3 = e.target.classList.contains('MuiBox-root');
+  //   var b4 = e.target.classList.contains('bob');
 
-    {this.state.isAuthenticated? <ListItem button onClick={this.handleLogout} style={{marginLeft:"0px", height:"50px",color:"white"}} primary="logout" > <ListItemText primary="LOGOUT" /> </ListItem>:  <ListItem button component={Link}  to='/' style={{marginLeft:"20px", height:"50px",color:"white"}} primary="login" > <ListItemText primary="LOGIN" /> </ListItem>}
+  //   if (b1 || b2 || b3 || b4 ){
+  //     // console.log("box is close")
+  //     this.setState({openNav:false});
+  //   } else {
+  //     // console.log('not closed')
+  //   }
+  // }
 
-      </List>
-   )
+  handleOpenNavLeft (event) {
+    this.setState({
+      openNavLeft: event.currentTarget
+    });
+  };
+
+  handleCloseNavLeft (cb = () => {}) {
+    this.setState({
+      openNavLeft: null
+    })
   }
 
-  openNav(){
-    this.setState({openNav:!this.state.openNav})
-  }  detectOffClick(e){
-    var b1 = e.target.classList.contains('MuiTypography-root');
-    var b2 = e.target.classList.contains('MuiList-root');
-    var b3 = e.target.classList.contains('MuiBox-root');
-    var b4 = e.target.classList.contains('bob');
+  handleOpenNavRight (event) {
+    this.setState({
+      openNavRight: event.currentTarget
+    });
+  };
 
-    if (b1 || b2 || b3 || b4 ){
-      // console.log("box is close")
-      this.setState({openNav:false});
-    } else {
-      // console.log('not closed')
-    }
+  handleCloseNavRight (cb = () => {}) {
+    this.setState({
+      openNavRight: null
+    })
   }
+
   render() {
 
+    var displayUser = this.props.cookieData ? this.props.cookieData.username :"Login";
     return (
       <Box sx={{ flexGrow: 1 }}>
         <AppBar position="static">
@@ -99,13 +115,26 @@ class NavBar extends React.Component {
               color="inherit"
               aria-label="menu"
               sx={{ mr: 2 }}
+              onClick={this.handleOpenNavLeft}
             >
               <MenuIcon />
+              <DisplayMenuLeft
+                anchorEl={this.state.openNavLeft}
+                handleClose={this.handleCloseNavLeft}
+              />
             </IconButton>
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
               Splitsy
             </Typography>
-            <Button color="inherit" onClick={this.displayNav}>Login/User</Button>
+            <Button edge="end" color="inherit" aria-haspopup="true" aria-expanded="true" aria-controls="composition-menu" onClick={this.handleOpenNavRight}>
+              {displayUser}
+            </Button>
+            <DisplayMenuRight
+              anchorEl={this.state.openNavRight}
+              handleClose={this.handleCloseNavRight}
+              handleLogout={this.handleLogout}
+            />
+
           </Toolbar>
         </AppBar>
         <Outlet />
