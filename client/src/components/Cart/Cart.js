8ff9190d7860ad3712afa064a7e6ca78e1325fa0
Tan Ha <79884390+tandha@users.noreply.github.com>
2022-06-11 05:39:06 -0700

    cart is styled and looked better

--- client/src/components/Cart/Cart.js
+++ client/src/components/Cart/Cart.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 // import ButtonAppBar from './navbar.js';
-import OrderCode from './orderCode.js';
+// import OrderCode from './orderCode.js';
 import RedirectButton from './redirectButton.js';
 import ItemList from './itemList.js';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
@@ -10,15 +10,14 @@ import Summary from './summary.js';
 import { useLocation } from 'react-router-dom';
 import $ from 'jquery';
 import { useNavigate } from "react-router-dom";
-
-
+import menuStyles from './cartStyles.css';
 
 function Cart(props) {
-  var getCookie = function(name){
-    if ( document.cookie){
-     return {[name]: document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)')?.pop() || ''}
+  var getCookie = function (name) {
+    if (document.cookie) {
+      return { [name]: document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)')?.pop() || '' }
     } else {
-      return {[name]:''}
+      return { [name]: '' }
     }
   }
   let navigate = useNavigate();
@@ -32,70 +31,70 @@ function Cart(props) {
   console.log('new window log: ', getCookie('Session'));
   // console.log('current props inside Cart.js is: ', props);
 
- var updateCartDatabase = () => {
-   // update database logic here, then send function to RedirectButton
-  //  var session_id = 1;
-  var link = `/session/update_cart`;
-  $.ajax({
-    method: "POST",
-    url: link,
-    // contentType: 'text/plain',
-    data: {
-      cart: data,
-      // totalTax: totalTax,
-      // grandTotal: grandTotal,
-    },
-    headers:{'Authorization':'Bearer ' + getCookie('Session').Session},
-    success: (response) => {
-      if (response === 'POST cart request received!') {
-        console.log('POST cart request success!');
+  var updateCartDatabase = () => {
+    // update database logic here, then send function to RedirectButton
+    //  var session_id = 1;
+    var link = `/session/update_cart`;
+    $.ajax({
+      method: "POST",
+      url: link,
+      // contentType: 'text/plain',
+      data: {
+        cart: data,
+        // totalTax: totalTax,
+        // grandTotal: grandTotal,
+      },
+      headers: { 'Authorization': 'Bearer ' + getCookie('Session').Session },
+      success: (response) => {
+        if (response === 'POST cart request received!') {
+          console.log('POST cart request success!');
+        }
+
+      },
+      error: (error) => {
+        console.log('Error: ', error);
+        navigate(`/protected/RestaurantList`);
       }
-
-    },
-    error: (error) => {
-      console.log('Error: ', error);
-      navigate(`/protected/RestaurantList`);
-    }
-  })
-  .done(function() {
-    console.log("cart ajax call is done!");
-  });
- }
+    })
+      .done(function () {
+        console.log("cart ajax call is done!");
+      });
+  }
 
 
-function updateSummaryDatabase() {
-  // update database logic here, then send function to RedirectButton
-  var session_id = 1;
- var link = `/session${session_id}/update_summary`;
- $.ajax({
-   method: "POST",
-   url: link,
-   // contentType: 'text/plain',
-   data: {
-     cart: data,
-    //  totalTax: totalTax,
-    //  grandTotal: grandTotal,
-     // session_id: session_id
-   },
-   success: (response) => {
-     if (response === 'POST summary request received!') {
-       console.log('POST summary request success!');
-     }
+  function updateSummaryDatabase() {
+    // update database logic here, then send function to RedirectButton
+    var session_id = 1;
+    var link = `/session${session_id}/update_summary`;
+    $.ajax({
+      method: "POST",
+      url: link,
+      // contentType: 'text/plain',
+      data: {
+        cart: data,
+        //  totalTax: totalTax,
+        //  grandTotal: grandTotal,
+        // session_id: session_id
+      },
+      success: (response) => {
+        if (response === 'POST summary request received!') {
+          console.log('POST summary request success!');
+        }
 
-   },
-   statusCode: {
-     200: function() {
-       console.log( "Status Code 200 ajax cart request!" );
-     }
-   },
-   error: (err) => {
-     console.log('Error: ', err);
-   }
- })
- .done(function() {
-   console.log("cart ajax call is done!");
- });
-}
+      },
+      statusCode: {
+        200: function () {
+          console.log("Status Code 200 ajax cart request!");
+        }
+      },
+      error: (err) => {
+        console.log('Error: ', err);
+      }
+    })
+      .done(function () {
+        console.log("cart ajax call is done!");
+      });
+  }
 
   return (
     <div >
@@ -103,10 +102,12 @@ function updateSummaryDatabase() {
         <ShoppingCartIcon />
         Cart
       </h1>
-      <OrderCode />
-      <ItemList cart={data}/>
-      <Summary cart={data} />
-      <RedirectButton updateCartDatabase={updateCartDatabase} updateSummaryDatabase={updateSummaryDatabase} />
+      {/* <OrderCode /> */}
+      <div className='cart' >
+        <ItemList className='item-list' cart={data} />
+        <Summary className='summary' cart={data} />
+        <RedirectButton className='redirect-button' updateCartDatabase={updateCartDatabase} updateSummaryDatabase={updateSummaryDatabase} />
+      </div>
     </div >
   )
 }
--- client/src/components/Cart/redirectButton.js
+++ client/src/components/Cart/redirectButton.js
@@ -4,13 +4,13 @@ import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
 import Stack from '@mui/material/Stack';
 import { Link } from "react-router-dom";
-
+import RestaurantIcon from '@mui/icons-material/Restaurant';
 
 const RedirectButton = (props) => {
 
   return (
     <Stack direction="row" spacing={2}>
-      <Button variant="contained" endIcon={<ShoppingCartIcon fontSize="large" />}>
+      <Button variant="contained" endIcon={<RestaurantIcon fontSize="large" />}>
         <Link to="/protected/Restaurant/pick" style={{ 'textDecoration': 'none', color: 'white' }}>
           Select Restaurant
         </Link>
@@ -20,12 +20,10 @@ const RedirectButton = (props) => {
           Back to Menu
         </Link>
       </Button>
-      <Button onClick={props.updateCartDatabase} variant="contained" endIcon={<ArrowCircleRightIcon />}>
-
+      {/* <Button onClick={props.updateCartDatabase} variant="contained" endIcon={<ArrowCircleRightIcon />}>
         update cart
-
-      </Button>
-      <Button variant="contained" endIcon={<ArrowCircleRightIcon />}>
+      </Button> */}
+      <Button onClick={props.updateCartDatabase} variant="contained" endIcon={<ArrowCircleRightIcon />}>
         <Link to="/protected/Payment" style={{ 'textDecoration': 'none', color: 'white' }} >
         Checkout
         </Link>
